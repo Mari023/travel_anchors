447c27e00bd5a3bbfed4ecf9f7da77249f160f63
Mari023 <Mari_023@outlook.de>
2020-10-17 11:20:30 +0200

    setup dev environment for fabric

---
+++ src/main/java/de/castcrafter/travel_anchors_fabric/TravelAnchors.java
@@ -0,0 +1,10 @@
+package de.castcrafter.travel_anchors_fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TravelAnchors implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/castcrafter/travel_anchors_fabric/client/TravelAnchorsClient.java
@@ -0,0 +1,13 @@
+package de.castcrafter.travel_anchors_fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TravelAnchorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
